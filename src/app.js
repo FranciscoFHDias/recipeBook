ff73a7e5a95ba173e9ed1e1332a546c00cb40545
Francisco Dias <francisco.ffbhd@gmail.com>
2019-10-29 23:31:13 +0000

    front-end hooked

--- src/app.js
+++ src/app.js
@@ -1,3 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { HashRouter, Route, Switch  }
+import { HashRouter, Route, Switch } from 'react-router-dom'
+
+import Home from './components/pages/home'
+
+class App extends React.Component {
+  render() {
+    return(
+      <HashRouter>
+        <Switch>
+          <Route path='/' component={Home} />
+        </Switch>
+      </HashRouter>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
