c1acab488321a97b1a6b569d8db2088cee046ba5
Francisco Dias <francisco.ffbhd@gmail.com>
2019-11-01 16:56:10 +0000

    Adjust buildpacks on Heroku

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
-import Auth from '../../lib/Auth'
+import Auth from '../../lib/auth'
 class Login extends React.Component {
   constructor() {
     super()
--- src/components/common/secureRoute.js
+++ src/components/common/secureRoute.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route, Redirect } from 'react-router-dom'
 
-import Auth from '../../lib/Auth'
+import Auth from '../../lib/auth'
 
 const SecureRoute = (props) => {
   if(Auth.isAuthenticated()) return <Route {...props} />
